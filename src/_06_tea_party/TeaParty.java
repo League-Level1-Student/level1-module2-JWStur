2d132966d9bb66eac0a096b30b54327b0bb8986e
student <student@ip-172-31-63-198.us-west-2.compute.internal>
2021-06-26 13:43:37 -0700

    DONE!

--- src/_06_tea_party/TeaParty.java
+++ src/_06_tea_party/TeaParty.java
@@ -1,30 +1,41 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+package _06_tea_party;
 
 public class TeaParty {
 	public String welcome(String name, boolean isWoman, boolean isKnighted) {
 		String result = "";
+
+		if (isWoman && isKnighted)
+			result = "Hello Lady " + name;
 		
+		else if (isWoman && !isKnighted)
+			result = "Hello Ms. " + name;
+		
+		else if (!isWoman && isKnighted)
+			result = "Hello Sir " + name;
+		
+		else result = "Hello Mr. " + name;
+		
+		/*
 		if (isWoman == true) {
 			if (isKnighted == true) {
 				if (isWoman == true) {
 					result = "Hello Lady " + name;
-				}
-				else {
+				} else {
 					result = "Hello Sir " + name;
 				}
-			}
-			else {
+			} else {
 				if (isWoman == true) {
 					result = "Hello Ms " + name;
 				}
-				
+
 				else {
 					result = "Hello Mr " + name;
+				}
+
 			}
-				
-			}
-			
+
+		}
+		*/
 		return result;
 	}
 
--- src/_06_tea_party/TeaPartyRunner.java
+++
@@ -1,8 +0,0 @@
-package _06_tea_party;
-
-public class TeaPartyRunner {
-    public String welcome(String name, boolean isWoman, boolean isKnighted) {
-		return name, isWoman, isKnighted;
-        
-    }
-}
